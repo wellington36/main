5ef174daf2da546706eb4859cf6d0d0e721c18b6
Alexandre Rademaker <arademaker@gmail.com>
2020-04-30 13:19:13 -0300

    modidficacao dos codigos enviados:
    - 2.32: remocao de comentarios desnecessarios. espaço entre palavras e
    caracter de comentário. remoção de comentários de várias linhas do
    meio de um código.
    
    - 2.34: quebra de linha muito longa. melhor apresentacao dos comentários.

--- ex-2.32-b.rkt
+++ ex-2.32-b.rkt
@@ -1,35 +1,38 @@
 #lang racket
 
-;;Autor Caio Lins
+;; autor Caio Lins
 
-;;Tira os elementos repetidos da lista.
+;; tira os elementos repetidos da lista. A cada iteração, os elementos
+;; iguais ao elemento adicionado ao conjunto são excluídos utilizando
+;; a função <filter>.
+
 (define (make-set lst)
   (define (make-set-iter new-set lst)
     (if (null? lst)
-      new-set
-      (let ([element (car lst)])
-	(make-set-iter (cons element
-			     new-set)
-		       (filter (lambda (x) (not (= x element))) ;A cada iteração, os elementos
-			       (cdr lst))))))			;iguais ao elemento adicionado
-  (make-set-iter null lst))					;ao conjunto são excluídos  u- 
-								;tilizando a primitiva <filter>.
-;;Cria um conjunto com os elementos da lista
-;;dada e produz os subconjuntos, com uma ló-
-;;gica parecida  com  a do <count-change> do 
-;;capítulo 1.
+        new-set
+        (let ([element (car lst)])
+          (make-set-iter (cons element
+                               new-set)
+                         (filter (lambda (x) (not (= x element))) 
+                                 (cdr lst))))))			
+  (make-set-iter null lst))					
+								
+
+;; cria um conjunto com os elementos da lista dada e produz os
+;; subconjuntos, com uma ló- gica parecida com a do <count-change> do
+;; capítulo 1.
+
 (define (subsets s)
-  (let ([my-set (make-set s)])					;Criação do conjunto como vari-
-    (if (null? my-set)						;ável local.
-      (list null)
-      (let ((rest (subsets (cdr my-set))))
-	(append rest (map (lambda (x)
-			    (cons (car my-set)
-				  x))
-			  rest))))))
+  (let ([my-set (make-set s)])					
+    (if (null? my-set)						
+        (list null)
+        (let ((rest (subsets (cdr my-set))))
+          (append rest (map (lambda (x)
+                              (cons (car my-set) x))
+                            rest))))))
 
 
-;;Testes
-;(define my-list (list 1 2 2 3 4))
-;(make-set my-list)
-;(subsets my-list)
+
+;; testes
+
+(subsets (make-set (list 1 2 2 3 4)))
