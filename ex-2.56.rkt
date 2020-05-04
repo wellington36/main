3ffde210c1e61d0733d3b2cf3a8fec1be477cb96
Caio Lins <61877795+Caioflp@users.noreply.github.com>
2020-05-04 19:20:21 -0300

    Derivadas (#50)
    * adicionei definições de função anônima e status de primeira  classe
    
    * Mudei o os arquivos para .org e fiz as correções solicitadas no texto.
    
    * Corrigi definição de funções anônimas.
    
    * Adicionei outra solução para o exercício 2.32.
    
    * corrigi o termo 'predicado' (linha 16) para 'primtiva'.
    
    * adicionando exercício 2.39
    
    * adicionei exercício 2.39 e outra solução para o sudoku.
    
    * repus o exercício 2.32-b.
    
    * modifiquei arquivo 'derivadas.rkt' com a resposta para o ex-2.56.rkt
    
    * modifiquei arquivo 'derivadas.rkt' para ser a resposta do ex-2.56.
    
    * modifiquei arquivo 'derivadas.rkt' para ser a resposta do ex-2.56.

--- ex-2.56.rkt
+++ ex-2.56.rkt
@@ -7,9 +7,6 @@
   (and (variable? v1) (variable? v2)
        (eq? v1 v2)))
 
-;(define (make-sum a1 a2)
-; `(+ ,a1 ,a2))
-
 (define (=number? exp num)
   (and (number? exp) (= exp num)))
 
@@ -21,16 +18,12 @@
         (else
          `(+ ,a1 ,a2))))
 
-
 (define (addend s) (cadr s))
 
 (define (augend s) (caddr s))
 
 (define (sum? x)
   (and (pair? x) (eq? (car x) '+)))
-
-(define (make-product m1 m2)
-  `(* ,m1 ,m2))
 
 (define (make-product m1 m2)
   (cond ((or (=number? m1 0) (=number? m2 0)) 0)
@@ -40,14 +33,27 @@
          (* m1 m2))
         (else `(* ,m1 ,m2))))
 
-
-
 (define (multiplier p) (cadr p))
 
 (define (multiplicand p) (caddr p))
 
 (define (product? x)
   (and (pair? x) (eq? (car x) '*)))
+
+(define (make-exponentiation base exp)
+  (cond ([=number? exp 0] 1)
+	([=number? exp 1] base)
+	([and (number? base) (number? exp)] (expt base exp))
+	(else `(** ,base ,exp))))
+
+(define (exponentiation? x)
+  (and (pair? x) (eq? (car x) '**)))
+
+(define (base e)
+  (cadr e))
+
+(define (exponent e)
+  (caddr e))
 
 (define (deriv exp var)
   (cond ((number? exp) 0)
@@ -61,6 +67,19 @@
            (make-product (multiplier exp)
                          (deriv (multiplicand exp) var))
            (make-product (deriv (multiplier exp) var)
-                         (multiplicand exp))))
+                         (multiplicand exp)))) 
+	((exponentiation? exp)
+	 (make-product
+	   (make-product
+	     (exponent exp)
+	     (make-exponentiation (base exp) (make-sum
+					       (exponent exp)
+					       -1)))
+	   (deriv (base exp) var)))
         (else
          (error "unknown expression type -- DERIV" exp))))
+
+
+;;teste
+;
+;(deriv '(** x (+ x y)) 'x)
