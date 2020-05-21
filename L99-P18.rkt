16e746033b0cc3024e82d2c883133bfa668f52d1
Alexandre Rademaker <arademaker@gmail.com>
2020-05-21 10:17:57 -0300

    correcao test e melhora legibilidade

--- L99-P18.rkt
+++ L99-P18.rkt
@@ -2,15 +2,13 @@
 
 (require rackunit)
 
-;Questão 18
+; Given two indices, I and K, the slice is the list containing the
+; elements between the I'th and K'th element of the original list
+; (both limits included).  Start counting the elements with 1.
 
-;Given two indices, I and K, the slice is the list containing
-;the elements between the I'th and K'th element of the original list (both limits included).
-;Start counting the elements with 1.
-
-;Example:
-;* (slice '(a b c d e f g h i k) 3 7)
-;(C D E F G)
+; Example:
+; * (slice '(a b c d e f g h i k) 3 7)
+; (C D E F G)
 
 
 (define (select list n)
@@ -23,11 +21,11 @@
         ((> i 1) (slice  (cdr list) (- i 1) (- k 1)))
         (else (select list k))))
 
+(check-equal? (slice '(a b c d e f g h i k) 3 7)
+              '(c d e f g))
 
-(slice '(1 2 3 4 5 6 7 8 9) 2 8)
-(slice '(1 2 3 4 5 6 7 8 9 10 11 12 13) 3 10)
-(check-equal? (slice '(a b c d e f g h i k) 3 7) '(c d e f g))
-(check-equal? (slice '(a b c d e f g h i k l m n o) 4 9) '(c d e f g))
+(check-equal? (slice '(a b c d e f g h i k l m n o) 4 9)
+              '(d e f g h i))
 
 (provide slice)
 
