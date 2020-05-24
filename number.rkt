f1804ac2622d3b6ab4cee34861d29875acd1cda0
João Lucas Duim <61877928+JLD819@users.noreply.github.com>
2020-05-24 12:26:59 -0300

    Complex and numbers (#85)
    * number
    
    * complex-2

---
+++ number.rkt
@@ -0,0 +1,22 @@
+#lang racket
+
+(require "table.rkt")
+
+(put 'add '(scheme-number scheme-number) +)
+(put 'sub '(scheme-number scheme-number) -)
+(put 'mul '(scheme-number scheme-number) *)
+(put 'div '(scheme-number scheme-number) /)
+(put 'equ? '(scheme-number scheme-number) =)
+
+(define (=zero? x)
+  (= x 0))
+
+(put '=zero? '(scheme-number) =zero?)
+
+
+;; test
+
+(require rackunit)
+
+(check-equal? (=zero? 10) false)
+(check-equal? (=zero? 0) true)
