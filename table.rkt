8d614e23b1c06e7c770c461ef6f82b9f9adfc109
Caio Lins <61877795+Caioflp@users.noreply.github.com>
2020-05-25 13:11:50 -0300

    Add coerce table (#89)
    * adicionei definições de função anônima e status de primeira  classe
    
    * Mudei o os arquivos para .org e fiz as correções solicitadas no texto.
    
    * Corrigi definição de funções anônimas.
    
    * Adicionei outra solução para o exercício 2.32.
    
    * corrigi o termo 'predicado' (linha 16) para 'primtiva'.
    
    * adicionando exercício 2.39
    
    * adicionei exercício 2.39 e outra solução para o sudoku.
    
    * repus o exercício 2.32-b.
    
    * adiciona coerce-table

--- table.rkt
+++ table.rkt
@@ -2,6 +2,8 @@
 
 ;; https://docs.racket-lang.org/guide/hash-tables.html
 
+;--------------------------operations and types table---------------------------
+
 (define table (make-hash))
 
 (define (start)
@@ -24,5 +26,30 @@
   table)
 
 (provide put get show start)
+
+;--------------------------------coercion table---------------------------------
+
+(define coercion-table (make-hash))
+
+(define (start-coercion)
+  (set! coercion-table (make-hash)))
+
+(define (put-coercion type1 type2 proc-coercion)
+  (if (hash-has-key? coercion-table type1)
+      (hash-set! (hash-ref coercion-table type1) type2 proc-coercion)
+      (hash-set! coercion-table type1 (make-hash (list (cons type2 proc-coercion))))))
+
+(define (get-coercion type1 type2)
+  (if (hash-has-key? coercion-table type1)
+      (let ((tbo (hash-ref coercion-table type1)))
+	(if (hash-has-key? tbo type2)
+	    (hash-ref tbo type2)
+	    false))
+      false))
+
+(define (show-coercion)
+  coercion-table)
+
+(provide put-coercion get-coercion show-coercion show-coercion)
 
 
