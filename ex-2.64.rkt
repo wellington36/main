ae06bd98131777b337cf9d495d30fb83e511d726
wellington <wefinalfantasy@gmail.com>
2020-05-31 21:15:22 -0300

    modificações

--- ex-2.64.rkt
+++ ex-2.64.rkt
@@ -24,25 +24,6 @@
         (cons (make-tree this-entry left-tree right-tree)
               remaining-elts))))
 
-;; item A
 
 (list->tree '(1 3 5 7 9 11))
-; '(5 (1 () (3 () ())) (9 (7 () ()) (11 () ())))
-
-; A função constroi uma árvore onde cada nó se divide em 3
-; segmentos, onde o primeiro segmento tera o elemento central
-; da lista o segundo repitira a função com os elementos
-; anteriores ao central e o terceiro aplicara a função nos
-; elementos após o elemento central, de forma recursiva.
-
-; Quanto a escolha do elemento central se a lista tem um
-; número ímpar de elementos será o do meio, caso tenha n=2k
-; elementos será o elemento da possição k.
-
-;; item B
-
-; Note que, partial-tree faz uma recurção quebrando a lista
-; em outras 2, ou seja, list->tree é O(logn).
-
-
 
