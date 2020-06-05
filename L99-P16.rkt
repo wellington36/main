dd6573fd41b6174675d69c35b8b202e03101f538
Erick_EMAp <61877775+ErickEMAp@users.noreply.github.com>
2020-06-05 09:53:54 -0300

    99 problems (#108)

--- L99-P16.rkt
+++ L99-P16.rkt
@@ -6,23 +6,26 @@
 
 Drop every N'th element from a list.
 
-Example:
-* (drop '(a b c d e f g h i k) 3)
-(A B D E G H K)
-
 |#
 
-(define (drop a-list n)
-  (define (drop-aux a-list r-list n)
-    (cond ((empty? a-list) r-list)
-          ((= n 0) r-list)
-          (else (drop-aux (cdr a-list)
-                          (append  r-list (list (car a-list)))
-                          (- n 1)))))
-  (drop-aux a-list '() n))
+
+(define (drop list n)
+  (define (drop-aux list n i)
+    (if (empty? list)
+        '()
+        (cond ((< i n) (cons (car list) (drop-aux (cdr list) n (+ 1 i))))
+              (else (drop-aux (cdr list) n 1)))))
+  (drop-aux list n 1))
+
 
 (provide drop)
 
+
+;Tests
+
 (check-equal? (drop '(a b c d e f g h i k) 3)
               '(a b d e g h k))
+
+(check-equal? (drop '(1 2 3) 2)
+              '(1 3))
 
