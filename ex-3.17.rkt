928a82814d0ba8971d71dfbd694616cbadfb460c
Caio Lins <caiolinspeixoto@gmail.com>
2020-06-07 19:04:52 -0300

    add novo count-pairs

--- ex-3.17.rkt
+++ ex-3.17.rkt
@@ -1,1 +1,13 @@
 #lang sicp
+
+(define count-pairs
+  (let ((visited-pairs nil))
+    (lambda (x)
+      (if (pair? x)
+	  (if (memq x visited-pairs)
+	      0
+	      (begin (set! visited-pairs (cons x visited-pairs))
+		     (+ (count-pairs (car x))
+			(count-pairs (cdr x))
+			1)))
+	  0))))
