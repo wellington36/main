d1056ceecae456f65c24b93ef81e7eec1de47582
Caio Lins <caiolinspeixoto@gmail.com>
2020-06-08 13:58:08 -0300

    add comentarios na versao bugada do ex-3.17

--- ex-3.17-bug.rkt
+++ ex-3.17-bug.rkt
@@ -1,5 +1,10 @@
 #lang sicp
 
+;; Essa versão do código contém um bug. Quando o procedimento `count-pairs` é
+;; chamado duas vezes seguidas com o mesmo input (mesmo no sentido de `eq?`),
+;; o histórico `visited-pairs` não é reiniciado, de modo que a segunda chamada
+;; retorna sempre 0. 
+
 (define count-pairs
   (let ((visited-pairs nil))
     (lambda (x)
@@ -11,3 +16,9 @@
 			(count-pairs (cdr x))
 			1)))
 	  0))))
+
+;; Teste do bug
+
+(define z (cons 1 (cons 2 (cons 3 nil))))
+(count-pairs z)
+(count-pairs z)
